e6fa59efbc19db3349cf7017fa320019d6774573
Subhan <squadiloglu@gmail.com>
2022-11-15 17:31:32 +0400

    Dockerfile added

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.10.4-alpine
+WORKDIR /linkedin-challenge
+COPY .. /linkedin-challenge
+RUN /usr/local/bin/python -m pip install --upgrade pip
+RUN pip install -r /linkedin-challenge/requirements.txt --no-cache-dir
+EXPOSE 8080
+CMD ["python", "main.py"]
--- linkedin_rg.py
+++ linkedin_rg.py
@@ -5,7 +5,7 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-import pyautogui
+
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -43,18 +43,5 @@ time.sleep(3)
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
     (By.XPATH, "//button[text()='Продолжить']"))).click()
 
-# phone verification
-time.sleep(5)
-WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it
-                                ((By.XPATH, "//iframe[@title='Проверка безопасности']")))
-time.sleep(3)
-WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "phonenumber"))).send_keys(phone_number)
-
-time.sleep(5)
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
-    (By.XPATH, "//button[text()='Отправить']"))).click()
-time.sleep(20)
-verify_code = input("Enter verification code from SMS:\n")
-
-
+# after captcha
 print("LinkedIn account created")
